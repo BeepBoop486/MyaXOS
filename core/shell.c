268373600d41e04f708088f7c65b50b977dc6d0d
Mya <sustix123@gmail.com>
2019-12-27 18:22:52 -0500

    Fixed cd and cat, added ls and echo commands to the shell

--- core/shell.c
+++ core/shell.c
@@ -34,49 +34,68 @@ void start_shell() {
 			/**
 			 * Execute command
 			 */
-			if(!strcmp(cmd, "cd")) {
-				if(tokenid < 2) {
+
+			if (!strcmp(cmd, "cd")) {
+				if (tokenid < 2) {
 					kprintf("cd: argument expected\n");
 					continue;
 				} else {
-					if(argv[1][0] == '/') {
+					if (argv[1][0] == '/') {
 						fs_node_t * chd = kopen(argv[1], 0);
-
-						if(chd) {
+						if (chd) {
 							node = chd;
 							memcpy(path, argv[1], strlen(argv[1]));
 							path[strlen(argv[1])] = '\0';
 						} else {
-							kprintf("cd: couldn't change directory\n");
+							kprintf("cd: could not change directory\n");
 						}
 					}
 				}
-			} else if(!strcmp(cmd, "cat")) {
-				if(tokenid < 2) {
+			} else if (!strcmp(cmd, "cat")) {
+				if (tokenid < 2) {
 					kprintf("cat: argument expected\n");
 					continue;
 				} else {
-					if(argv[1][0] == '/') {
+					if (argv[1][0] == '/') {
 						fs_node_t * file = kopen(argv[1],0);
-
-						if(!file) {
+						if (!file) {
 							kprintf("cat: could not open file `%s`\n", argv[1]);
 							continue;
 						}
-
 						char *bufferb = malloc(file->length + 200);
 						size_t bytes_read = read_fs(file, 0, file->length, (uint8_t *)bufferb);
 						size_t i = 0;
-						
-						for(i = 0; i < bytes_read; ++i) {
+						for (i = 0; i < bytes_read; ++i) {
 							writech(bufferb[i]);
 						}
 						free(bufferb);
 						close_fs(file);
-
 					}
 				}
+			} else if (!strcmp(cmd, "echo")) {
+				if (tokenid < 2) {
+					continue;
+				} else {
+					int i = 1;
+					for (i = 1; i < tokenid; ++i) {
+						kprintf("%s ", argv[i]);
+					}
+					kprintf("\n");
+				}
+			} else if (!strcmp(cmd, "ls")) {
+				struct dirent * entry = NULL;
+				int i = 0;
+				entry = readdir_fs(node, i);
+				while (entry != NULL) {
+					kprintf("%s\n", entry->name);
+					free(entry);
+					i++;
+					entry = readdir_fs(node, i);
+				}
+			} else {
+				kprintf("Unrecognized command: %s\n", cmd);
 			}
+			
 		}
 	}
 }
