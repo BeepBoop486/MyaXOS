8de580a72e677f6758800231edac4f7a73b43e1e
Mya <sustix123@gmail.com>
2019-12-27 18:27:06 -0500

    Relative paths are now supported in the shell

--- core/shell.c
+++ core/shell.c
@@ -30,72 +30,91 @@ void start_shell() {
 				pch = strtok_r(NULL, " ", &save);
 			}
 			argv[tokenid] = NULL;
-			
+
 			/**
 			 * Execute command
 			 */
 
-			if (!strcmp(cmd, "cd")) {
-				if (tokenid < 2) {
-					kprintf("cd: argument expected\n");
-					continue;
-				} else {
-					if (argv[1][0] == '/') {
-						fs_node_t * chd = kopen(argv[1], 0);
-						if (chd) {
-							node = chd;
-							memcpy(path, argv[1], strlen(argv[1]));
-							path[strlen(argv[1])] = '\0';
-						} else {
-							kprintf("cd: could not change directory\n");
-						}
-					}
-				}
-			} else if (!strcmp(cmd, "cat")) {
-				if (tokenid < 2) {
-					kprintf("cat: argument expected\n");
-					continue;
-				} else {
-					if (argv[1][0] == '/') {
-						fs_node_t * file = kopen(argv[1],0);
-						if (!file) {
-							kprintf("cat: could not open file `%s`\n", argv[1]);
-							continue;
-						}
-						char *bufferb = malloc(file->length + 200);
-						size_t bytes_read = read_fs(file, 0, file->length, (uint8_t *)bufferb);
-						size_t i = 0;
-						for (i = 0; i < bytes_read; ++i) {
-							writech(bufferb[i]);
-						}
-						free(bufferb);
-						close_fs(file);
-					}
-				}
-			} else if (!strcmp(cmd, "echo")) {
-				if (tokenid < 2) {
-					continue;
-				} else {
-					int i = 1;
-					for (i = 1; i < tokenid; ++i) {
-						kprintf("%s ", argv[i]);
-					}
-					kprintf("\n");
-				}
-			} else if (!strcmp(cmd, "ls")) {
-				struct dirent * entry = NULL;
-				int i = 0;
-				entry = readdir_fs(node, i);
-				while (entry != NULL) {
-					kprintf("%s\n", entry->name);
-					free(entry);
-					i++;
-					entry = readdir_fs(node, i);
-				}
-			} else {
-				kprintf("Unrecognized command: %s\n", cmd);
-			}
-			
+			 if (!strcmp(cmd, "cd")) {
+ 				if (tokenid < 2) {
+ 					kprintf("cd: argument expected\n");
+ 					continue;
+ 				} else {
+ 					char * filename = malloc(sizeof(char) * 1024);
+ 					if (argv[1][0] == '/') {
+ 						memcpy(filename, argv[1], strlen(argv[1]) + 1);
+ 					} else {
+ 						memcpy(filename, path, strlen(path));
+ 						if (!strcmp(path,"/")) {
+ 							memcpy((void *)((uintptr_t)filename + strlen(path)),argv[1],strlen(argv[1])+1);
+ 						} else {
+ 							filename[strlen(path)] = '/';
+ 							memcpy((void *)((uintptr_t)filename + strlen(path) + 1),argv[1],strlen(argv[1])+1);
+ 						}
+ 					}
+ 					fs_node_t * chd = kopen(filename, 0);
+ 					if (chd) {
+ 						node = chd;
+ 						memcpy(path, filename, strlen(filename));
+ 						path[strlen(filename)] = '\0';
+ 					} else {
+ 						kprintf("cd: could not change directory\n");
+ 					}
+ 				}
+ 			} else if (!strcmp(cmd, "cat")) {
+ 				if (tokenid < 2) {
+ 					kprintf("cat: argument expected\n");
+ 					continue;
+ 				} else {
+ 					char * filename = malloc(sizeof(char) * 1024);
+ 					if (argv[1][0] == '/') {
+ 						memcpy(filename, argv[1], strlen(argv[1]) + 1);
+ 					} else {
+ 						memcpy(filename, path, strlen(path));
+ 						if (!strcmp(path,"/")) {
+ 							memcpy((void *)((uintptr_t)filename + strlen(path)),argv[1],strlen(argv[1])+1);
+ 						} else {
+ 							filename[strlen(path)] = '/';
+ 							memcpy((void *)((uintptr_t)filename + strlen(path) + 1),argv[1],strlen(argv[1])+1);
+ 						}
+ 					}
+ 					fs_node_t * file = kopen(filename,0);
+ 					if (!file) {
+ 						kprintf("cat: could not open file `%s`\n", argv[1]);
+ 						continue;
+ 					}
+ 					char *bufferb = malloc(file->length + 200);
+ 					size_t bytes_read = read_fs(file, 0, file->length, (uint8_t *)bufferb);
+ 					size_t i = 0;
+ 					for (i = 0; i < bytes_read; ++i) {
+ 						writech(bufferb[i]);
+ 					}
+ 					free(bufferb);
+ 					close_fs(file);
+ 				}
+ 			} else if (!strcmp(cmd, "echo")) {
+ 				if (tokenid < 2) {
+ 					continue;
+ 				} else {
+ 					int i = 1;
+ 					for (i = 1; i < tokenid; ++i) {
+ 						kprintf("%s ", argv[i]);
+ 					}
+ 					kprintf("\n");
+ 				}
+ 			} else if (!strcmp(cmd, "ls")) {
+ 				struct dirent * entry = NULL;
+ 				int i = 0;
+ 				entry = readdir_fs(node, i);
+ 				while (entry != NULL) {
+ 					kprintf("%s\n", entry->name);
+ 					free(entry);
+ 					i++;
+ 					entry = readdir_fs(node, i);
+ 				}
+ 			} else {
+ 				kprintf("Unrecognized command: %s\n", cmd);
+ 			}
 		}
 	}
 }
