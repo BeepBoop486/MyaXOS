e6b1a0e656d10b3b249da9bd32cf34a6f55bbab1
Mya <sustix123@gmail.com>
2019-12-27 16:24:04 -0500

    Ext2 larger support

--- main.c
+++ main.c
@@ -53,7 +53,22 @@ int main(struct multiboot *mboot_ptr) {
 	uint32_t bytes_read;
 	bytes_read = read_fs(test_file, 0, 255, &buffer);
 	kprintf("cat /etc/kernel/hello.txt\n");
-	kprintf("%s", buffer);
+	uint32_t i = 0;
+	for(i = 0; i < bytes_read; ++i) {
+		kprintf("%c", buffer[i]);
+	}
+	close_fs(test_file);
+	free(test_file);
+	test_file = kopen("/usr/docs/README.txt", NULL);
+	char * bufferb = malloc(test_file->length + 200);
+	bytes_read = read_fs(test_file, 100, test_file->length, bufferb);
+	for(i = 0; i < bytes_read; ++i) {
+		kprintf("%c", (char)bufferb[i]);
+		if(i % 500 == 0) {
+			timer_wait(30);
+		}
+	}
+	free(test_file);
 	close_fs(test_file);
 
 #if 0
